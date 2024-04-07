ac5f2fd59c3c354d75ba7437b1fb2a0db0a14f8a
MathewAviles <juanaviles12345@gmail.com>
2024-04-07 14:33:29 -0500

    tercer commit

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+#IMPORTS
 from flask import Flask,render_template,request,redirect,url_for
 
+#APP INIT
 app= Flask(__name__)
 
+
+
+#ROUTES
 @app.route('/')
 def inicio():
-    return render_template('index.html')
+    return render_template('base.html')
 
 
-
+#RUN APLICATION
 if __name__ == "__main__":
     app.run(debug=True)
