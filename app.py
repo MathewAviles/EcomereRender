c6d2280aaca7a9b9e7564c54f095d8604f3e06e8
MathewAviles <juanaviles12345@gmail.com>
2024-04-08 17:23:18 -0500

    sesiones de log in y sing in completas commit con bd render2

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(10), nullable=False, default='normal')  # 'normal' or 'admin'
     def __init__(self, username, email, password, role='normal'):
         self.username = username
